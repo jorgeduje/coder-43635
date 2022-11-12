77937eb5aca5ca3adc577f507f99e016a4c545aa
jorgeduje <julian.russo@team.wirall.com>
2022-11-12 15:46:06 -0300

    cambios en app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Curso coder 43635
         </a>
       </header>
     </div>
