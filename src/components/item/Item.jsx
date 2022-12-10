eb154c75529e97f0d274ecf9a4fe4a6219c9480a
jorgeduje <julian.russo@team.wirall.com>
2022-12-10 10:34:24 -0300

    se mejora el estilo de la card

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,18 +1,49 @@
-
-import {Link} from "react-router-dom"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material"
+import { Link } from "react-router-dom"
 import "./Item.css"
 
-const Item = ({element}) => {
-
-
+const Item = ({ element }) => {
   return (
-    <div className="card">
-      <h2>{element.name}</h2>
-      <img src={element.img} alt="" />
-      <h3>${element.price}</h3>
-      <Link to={`/itemDetail/${element.id}`}>Ver detalle</Link>
-    </div>
-  );
-};
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="200"
+        image={element.img}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          color="primary"
+          align="center"
+        >
+          {element.name}
+        </Typography>
+        <Typography variant="body1" color="text.secondary" align="center">
+          {element.description}
+        </Typography>
+        <Typography variant="body1" color="secondary" align="center">
+          ${element.price}.-
+        </Typography>
+      </CardContent>
+      <CardActions style={{ display: "flex", justifyContent: "center" }}>
+        <Link to={`/itemDetail/${element.id}`} style={{textDecoration: "none"}}>
+          <Button size="large" variant="contained">
+            Ver detalle
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
+  )
+}
 
-export default Item;
+export default Item
