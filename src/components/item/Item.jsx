3b0f9543395740436ad33992a970c47462267bef
jorgeduje <julian.russo@team.wirall.com>
2022-12-10 10:35:04 -0300

    minor fix en link para ir a ver el detalle de producto

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,14 +1,18 @@
-
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import styles from "./Item.module.css"
 import imagenZapatilla from "../../assets/img/descarga.jpg"
 
+import {
+  Typography,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+} from "@mui/material"
 
-import { Typography , Box, Card, CardActions, CardContent, CardMedia,Button } from "@mui/material"
-
-const Item = ({element}) => {
-
-
+const Item = ({ element }) => {
   return (
     // <Box className={styles.card}>
     //   <h2 style={ {color: "red"} } >{element.name}</h2>
@@ -27,21 +31,34 @@ const Item = ({element}) => {
         alt="green iguana"
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div" color="primary" align="center">
-        {element.name}
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          color="primary"
+          align="center"
+        >
+          {element.name}
         </Typography>
         <Typography variant="body1" color="text.secondary" align="center">
-        {element.description}
+          {element.description}
         </Typography>
         <Typography variant="body1" color="secondary" align="center">
-        ${element.price}.-
+          ${element.price}.-
         </Typography>
       </CardContent>
-      <CardActions style={{display: "flex", justifyContent:"center"}}>
-        <Button size="large" variant="contained">Agregar al carrito</Button>
+      <CardActions style={{ display: "flex", justifyContent: "center" }}>
+        <Link
+          to={`/itemDetail/${element.id}`}
+          style={{ textDecoration: "none" }}
+        >
+          <Button size="large" variant="contained">
+            Ver detalle
+          </Button>
+        </Link>
       </CardActions>
     </Card>
-  );
-};
+  )
+}
 
-export default Item;
+export default Item
